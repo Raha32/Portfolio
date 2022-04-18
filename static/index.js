94660a0d26b0505ce3347fbd28bb42e6dc07dd75
raha32 <rayannhadid101@gmail.com>
2022-04-18 07:34:59 +0200

    small update js

--- static/index.js
+++ static/index.js
@@ -1,24 +1,37 @@
 function initElement(){
 	// initializing competences tab
-	var lang = document.getElementById("lang");
-	var ide = document.getElementById("ide");
-	var ideprog = document.getElementById("ideprog");
-	var langprog = document.getElementById("langprog");
-	var rightslide = document.getElementById("rightslide");
-	var leftslide = document.getElementById("leftslide");
-	var img = document.getElementById("img");
-	var imgchild = img.children;
+	const lang = document.getElementById("lang");
+	const ide = document.getElementById("ide");
+	const ideprog = document.getElementById("ideprog");
+	const langprog = document.getElementById("langprog");
+	const rightslide = document.getElementById("rightslide");
+	const leftslide = document.getElementById("leftslide");
+	const img = document.getElementById("img");
+	const imgide = document.getElementById("imgide");
+	const imgidechild = imgide.children;
+	const imgchild = img.children;
 	
 	// initializing indexes for languages
 	let i = 0;
-	let i_x = 0	
-	
+	let i_x = 0;
+	let i_i = 0;
+
+	while (i < imgidechild.length){
+		imgidechild[i].style.display = "none";
+		i++;
+		i_i++;
+	}
+	i = 0;
+
 	while (i < imgchild.length){
 		imgchild[i].style.display = "none";
 		i++;
 		i_x++;
 	}
 
+	if (ide.className == "active"){
+		alert("true");
+	}
 	// index for languages 
 	i = 0;
 	i_x--;
@@ -33,15 +46,20 @@ function initElement(){
 
 	function showLang(){
 		lang.style.display = "block";
+		lang.className="active";
+		ide.classList.remove("active");
 		ide.style.display = "none";
 	}
 
 	function showIde(){
 		lang.style.display = "none";
+		ide.className="active";
+		lang.classList.remove("active");
 		ide.style.display = "block";
 	}
 
 	function slideright(){
+
 		if ((i < i_x) && !(i > i_x)){
 			imgchild[i].style.display = "none";
 			i++;
